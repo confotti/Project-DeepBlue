7684dd8340bd343b2beb3ad00ee767db0925cd48
Lukas <lukas.almgren03@gmail.com>
2025-06-29 16:20:35 +0200

    Input system is up and running.

---
+++ Assets/Scripts/Inputs/PlayerInputHandler.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private DefaultInputActions defaultInputActions;
+    private InputAction movement;
+    private InputAction look;
+
+    public Vector2 Move { get { return movement.ReadValue<Vector2>(); } }
+    public Vector2 Look { get { return look.ReadValue<Vector2>(); } }
+    public Action OnInteract;
+    public Action OnJump;
+
+    private void Awake()
+    {
+        defaultInputActions = new DefaultInputActions();
+    }
+
+    private void OnEnable()
+    {
+        movement = defaultInputActions.PlayerMovement.Move;
+        movement.Enable();
+
+        look = defaultInputActions.PlayerMovement.Look;
+        look.Enable();
+
+        //Subscriptions
+        defaultInputActions.PlayerMovement.Interact.performed += Interact;
+        defaultInputActions.PlayerMovement.Jump.performed += Jump;
+    }
+
+    private void OnDisable()
+    {
+        //Unsubscriptions
+        defaultInputActions.PlayerMovement.Interact.performed -= Interact;
+        defaultInputActions.PlayerMovement.Jump.performed -= Jump;
+    }
+
+    private void Interact(InputAction.CallbackContext context)
+    {
+        OnInteract?.Invoke();
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        OnJump?.Invoke();
+    }
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,43 @@
 using UnityEngine;
 
-public class PlayerMovement
+public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private float speed = 10;
+
+    //References
+    private PlayerInputHandler inputHandler;
+    private Rigidbody rb;
+
+    private States currentState = States.swimming;
+
+    //TODO: Fix real statemachine
+    public enum States
+    {
+        standing,
+        swimming
+    }
+
+    private void Awake()
+    {
+        inputHandler = GetComponent<PlayerInputHandler>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        var move = inputHandler.Move;
+        Debug.Log(move);
+        rb.linearVelocity = new Vector3(move.x, 0, move.y) * speed;
+    }
+
 }
