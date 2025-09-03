90f5d9b166857a5f4816b1510a76695ed2fa5b0a
Lukas <83639816+Fiskface@users.noreply.github.com>
2025-09-03 17:31:31 +0200

    Playermovement changes
    -Implemented standing state (state machine needs to become a proper one still)
    -Added a jump function (needs grounded check)
    -Fixed mouse sensitivity at different angles bug.
    
    Need to look at mouse lagging around, might just be school mice, will see if it's better at home.

--- Assets/Scripts/Runtime/Player/PlayerMovement.cs
+++ Assets/Scripts/Runtime/Player/PlayerMovement.cs
@@ -1,19 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 10;
     [SerializeField] private float mouseSensitivity = 1;
+    [SerializeField] private float gravity = 20;
+    [SerializeField] private float jumpPower = 20;
 
-    private Vector2 eulerLook;
-    private float lookYMin = -90;
+    private Vector2 rotation;
     private float lookYMax = 90;
 
     //References
     private PlayerInputHandler inputHandler;
     private Rigidbody rb;
 
-    private States currentState = States.swimming;
+    [SerializeField] private States currentState = States.standing;
 
     //TODO: Fix real statemachine
     public enum States
@@ -32,37 +34,53 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnEnable()
     {
-        eulerLook.x = transform.rotation.eulerAngles.x;
-        eulerLook.y = transform.rotation.eulerAngles.y;
+        inputHandler.OnJump += OnJump;
+
+        rotation.x = transform.rotation.eulerAngles.y;
+        rotation.y = transform.rotation.eulerAngles.x;
     }
 
     private void OnDisable()
     {
-        
+        inputHandler.OnJump -= OnJump;
     }
 
     private void FixedUpdate()
     {
         Movement();
 
-        eulerLook.x -= inputHandler.Look.y * mouseSensitivity;
-        eulerLook.y += inputHandler.Look.x * mouseSensitivity;
-        eulerLook.y %= 360;
-        eulerLook.x = Mathf.Clamp(eulerLook.x, lookYMin, lookYMax);
-        transform.rotation = Quaternion.Euler(eulerLook);
+        rotation.x += inputHandler.Look.x * mouseSensitivity;
+        rotation.y += inputHandler.Look.y * mouseSensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, -lookYMax, lookYMax);
+        var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+        var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+        transform.rotation = xQuat * yQuat;
     }
 
     private void Movement()
     {
-        if(currentState  == States.swimming)
+        if (currentState == States.swimming)
         {
             rb.linearVelocity = (transform.rotation * new Vector3(inputHandler.Move.x, 0, inputHandler.Move.y)).normalized * speed;
         }
-        else
+        else if (currentState == States.standing)
         {
-            rb.linearVelocity = new Vector3(inputHandler.Move.x, 0, inputHandler.Move.y).normalized * speed;
+            var move = transform.rotation * new Vector3(inputHandler.Move.x, 0, inputHandler.Move.y);
+            move.y = 0;
+            move = move.normalized * speed;
+            move.y = rb.linearVelocity.y - gravity * Time.fixedDeltaTime;
+            rb.linearVelocity = move;
         }
-        
+
+    }
+
+    private void OnJump()
+    {
+        if (currentState != States.standing) return;
+
+        var move = rb.linearVelocity;
+        move.y = jumpPower;
+        rb.linearVelocity = move;
     }
 
 }
