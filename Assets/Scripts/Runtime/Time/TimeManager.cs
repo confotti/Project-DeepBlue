503fb01013d3be25825587f23d6b1bc41a65945b
Robin H <a22robhu@student.his.se>
2025-09-14 21:43:03 +0200

    HDRP

--- Assets/Scripts/Runtime/Time/TimeManager.cs
+++ Assets/Scripts/Runtime/Time/TimeManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal; 
+using UnityEngine.Rendering.HighDefinition; 
 
 public class TimeManager : MonoBehaviour
 {
