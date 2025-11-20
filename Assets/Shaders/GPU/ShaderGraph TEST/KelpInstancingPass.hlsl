703c4bb45b7dffc0df30e0a71de6c13204e668ec
confotti <robinhultberg157@gmail.com>
2025-11-20 15:43:47 +0100

    Tiktok tings and some kelp

---
+++ Assets/Shaders/GPU/ShaderGraph TEST/KelpInstancingPass.hlsl
@@ -0,0 +1,33 @@
+﻿struct Attributes
+{
+    float3 positionOS : POSITION;
+    float3 normalOS   : NORMAL;
+    uint instanceID   : SV_InstanceID;
+};
+
+struct Varyings
+{
+    float4 positionCS : SV_POSITION;
+    float3 normalWS   : TEXCOORD0;
+};
+
+Varyings Vert(Attributes IN)
+{
+    Varyings OUT;
+    float3 posWS;
+    float3 norWS;
+
+    // Call your existing ShaderGraph function:
+    Kelp_VertexTransform_float(
+        IN.positionOS,
+        IN.normalOS,
+        (float)IN.instanceID,
+        _WorldOffset,
+        posWS,
+        norWS
+    );
+
+    OUT.positionCS = TransformWorldToHClip(posWS);
+    OUT.normalWS = norWS;
+    return OUT;
+}
