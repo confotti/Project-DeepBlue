f941e1c07540416a0176b9f1d6d236cc41d815ce
confotti <robinhultberg157@gmail.com>
2026-01-16 12:52:16 +0100

    able to craft batteries/flashlight

--- Assets/Scripts/Runtime/Player/Flashlight.cs
+++ Assets/Scripts/Runtime/Player/Flashlight.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     public Light flashLight; 
+    public InventoryItemData flashlightItemData;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            flashLight.enabled = !flashLight.enabled; 
+            if (!GetComponent<PlayerInventoryHolder>().InventorySystem.ContainsItem(flashlightItemData, out var ab)) return;
+            flashLight.enabled = !flashLight.enabled;
         }
+
+        if (!GetComponent<PlayerInventoryHolder>().InventorySystem.ContainsItem(flashlightItemData, out var a)) flashLight.enabled = false;
     }
 }
