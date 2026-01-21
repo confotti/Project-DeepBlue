c37fb9ea3e17974678cd0bbd484eae900c48b4a5
Lukas <83639816+Fiskface@users.noreply.github.com>
2026-01-21 14:04:19 +0100

    Swimming now has acceleration

--- Assets/Scripts/Runtime/Player/PlayerSwimmingState.cs
+++ Assets/Scripts/Runtime/Player/PlayerSwimmingState.cs
@@ -6,14 +6,23 @@ public class PlayerSwimmingState : State<PlayerMovement>
 {
     [SerializeField] private float _swimmingSpeed = 20;
     [SerializeField] private float _swimmingFastSpeed = 40;
+    [SerializeField, Range(0f, 1f)] private float _accelaration = 0.1f;
 
     public override void PhysicsUpdate()
     {
         base.PhysicsUpdate();
 
+        var targetVel = (new Vector3(0, obj.InputHandler.SwimUp - obj.InputHandler.SwimDown, 0) +
+                    obj.CameraHead.transform.rotation * new Vector3(obj.InputHandler.Move.x, 0, obj.InputHandler.Move.y)).normalized *
+                    (obj.InputHandler.Run ? _swimmingFastSpeed : _swimmingSpeed);
+
+        obj.Rb.linearVelocity += (targetVel - obj.Rb.linearVelocity) * _accelaration;
+
+        /*
         obj.Rb.linearVelocity = (new Vector3(0, obj.InputHandler.SwimUp - obj.InputHandler.SwimDown, 0) +
-                obj.CameraHead.transform.rotation * new Vector3(obj.InputHandler.Move.x, 0, obj.InputHandler.Move.y)).normalized *
-                (obj.InputHandler.Run ? _swimmingFastSpeed : _swimmingSpeed);
+                    obj.CameraHead.transform.rotation * new Vector3(obj.InputHandler.Move.x, 0, obj.InputHandler.Move.y)).normalized *
+                    (obj.InputHandler.Run ? _swimmingFastSpeed : _swimmingSpeed);
+        */
     }
 
     public override void Enter()
