dfd26401c51647a20eaf1ebbc06f2d5e152568f0
confotti <robinhultberg157@gmail.com>
2026-02-18 16:01:48 +0100

    yippie

--- Assets/Scripts/Runtime/Inventory & Crafting Stuff/Item Scripts/ItemBehaviours/GlowstickItem.cs
+++ Assets/Scripts/Runtime/Inventory & Crafting Stuff/Item Scripts/ItemBehaviours/GlowstickItem.cs
@@ -4,18 +4,6 @@ public class GlowstickItem : ItemBehaviour
 {
     //Maybe make a throwable class that this can inherit from if we wanna throw a bunch of stuff. 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public override void PrimaryInput()
     {
         base.PrimaryInput();
