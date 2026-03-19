122acf11a98b991d5eaebe2d8f7211c7941e6e9c
Lukas <83639816+Fiskface@users.noreply.github.com>
2026-03-19 12:05:29 +0100

    guh

--- Assets/Scripts/Runtime/SUB/BiomeSubSplineHolder.cs
+++ Assets/Scripts/Runtime/SUB/BiomeSubSplineHolder.cs
@@ -7,18 +7,20 @@ public class BiomeSubSplineHolder : MonoBehaviour
     public static BiomeSubSplineHolder prevInstance;
 
 
-    public Spline entrySpline;
-    public Spline exitSpline;
+    public SplineContainer entrySpline;
+    public SplineContainer exitSpline;
 
 
     private void Awake()
     {
         prevInstance = nextInstance;
         nextInstance = this;
+
+        prevInstance.FixExitSpline(nextInstance.transform.TransformPoint(nextInstance.entrySpline.Spline[0].Position));
     }
 
     public void FixExitSpline(Vector3 whereTo)
     {
-
+        prevInstance.exitSpline.Spline.Add(new BezierKnot(whereTo));
     }
 }
