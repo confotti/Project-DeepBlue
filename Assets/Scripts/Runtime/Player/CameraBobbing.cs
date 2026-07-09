4e0ca90b90c2492d26afad389865a600fb11e7dd
Robin H <robinhultberg157@gmail.com>
2026-07-09 14:50:53 +0200

    Trying out CameraBobbing while walking/running

---
+++ Assets/Scripts/Runtime/Player/CameraBobbing.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class CameraBobbing : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private Transform cameraHead;
+
+    [Header("Walking")]
+    [SerializeField] private float walkBobAmount = 0.03f;
+    [SerializeField] private float walkBobSpeed = 8f;
+
+    [Header("Running")]
+    [SerializeField] private float runBobAmount = 0.08f;
+    [SerializeField] private float runBobSpeed = 12f;
+
+    [SerializeField] private float smoothness = 10f;
+
+
+    private Vector3 startPosition;
+    private float bobTimer;
+
+
+    private void Awake()
+    {
+        startPosition = cameraHead.localPosition;
+    }
+
+
+    private void LateUpdate()
+    {
+        ApplyBobbing();
+    }
+
+
+    private void ApplyBobbing()
+    {
+        Vector2 movement = playerMovement.InputHandler.Move;
+
+        bool moving = movement.magnitude > 0.1f;
+
+        if (!moving)
+        {
+            bobTimer = 0;
+
+            cameraHead.localPosition = Vector3.Lerp(
+                cameraHead.localPosition,
+                startPosition,
+                Time.deltaTime * smoothness
+            );
+
+            return;
+        }
+
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+
+        float amount = running ? runBobAmount : walkBobAmount;
+        float speed = running ? runBobSpeed : walkBobSpeed;
+
+
+        bobTimer += Time.deltaTime * speed;
+
+
+        float yOffset = Mathf.Sin(bobTimer) * amount;
+
+
+        Vector3 target = startPosition;
+        target.y += yOffset;
+
+
+        cameraHead.localPosition = Vector3.Lerp(
+            cameraHead.localPosition,
+            target,
+            Time.deltaTime * smoothness
+        );
+    }
+} 
