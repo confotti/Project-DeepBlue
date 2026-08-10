afef4bc0a1863a747af4fd78d502098da32283cb
Robin H <robinhultberg157@gmail.com>
2026-08-10 14:56:41 +0200

    Be able to take photos, they dont save tho :(

---
+++ Assets/Scripts/Runtime/Interfaces/PhotoCapture.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using System.Collections; 
+using UnityEngine.UI; 
+
+public class PhotoCapture : MonoBehaviour
+{
+    [Header("Photo Taker")]
+    [SerializeField] private Image photoDisplayArea;
+    [SerializeField] private GameObject photoFrame;
+    [SerializeField] private GameObject cameraUI; 
+
+    [Header("Flash Effect")]
+    [SerializeField] private GameObject cameraFlash;
+    [SerializeField] private float flashTime;
+
+    [Header("Flash Fader Effect")]
+    [SerializeField] private Animator fadingAnimation; 
+
+    private Texture2D screenCapture;
+    private bool viewingPhoto; 
+
+    private void Start()
+    {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!viewingPhoto)
+            {
+                StartCoroutine(CapturePhoto());
+            }
+            else
+            {
+                RemovePhoto(); 
+            }
+        }
+    }
+
+    void MakeBlackAndWhite()
+    {
+        Color[] pixels = screenCapture.GetPixels();
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            Color pixel = pixels[i];
+
+            float gray = pixel.r * 0.299f +
+                         pixel.g * 0.587f +
+                         pixel.b * 0.114f;
+
+            pixels[i] = new Color(gray, gray, gray, pixel.a);
+        }
+
+        screenCapture.SetPixels(pixels);
+        screenCapture.Apply();
+    }
+
+    IEnumerator CapturePhoto()
+    {
+        cameraUI.SetActive(false); 
+        viewingPhoto = true;
+
+        yield return new WaitForEndOfFrame();
+
+        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
+
+        screenCapture.ReadPixels(regionToRead, 0, 0, false);
+        screenCapture.Apply();
+        MakeBlackAndWhite(); 
+        ShowPhoto(); 
+    }
+
+    void ShowPhoto()
+    {
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        photoDisplayArea.sprite = photoSprite;
+
+        photoFrame.SetActive(true);
+        StartCoroutine(CameraFlashEffect());
+        fadingAnimation.Play("PhotoFade"); 
+    }
+
+    IEnumerator CameraFlashEffect()
+    {
+        cameraFlash.SetActive(true);
+        yield return new WaitForSeconds(flashTime);
+        cameraFlash.SetActive(false); 
+    }
+
+    void RemovePhoto()
+    {
+        viewingPhoto = false; 
+        photoFrame.SetActive(false);
+        cameraUI.SetActive(true); 
+    }
+}
