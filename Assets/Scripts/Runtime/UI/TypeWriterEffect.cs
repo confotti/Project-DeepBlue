99bb8ba9729787677603e6dc7b5965ea3ba2ab63
xMelancholiccx <elsa@glin.se>
2026-08-24 11:52:06 +0200

    greybox text

---
+++ Assets/Scripts/Runtime/UI/TypeWriterEffect.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using TMPro;
+
+
+[RequireComponent(typeof(TMP_Text))]
+public class TypeWriterEffect : MonoBehaviour
+{
+    private TMP_Text _textBox;
+
+    //prototyping
+    [Header("Test String")]
+    [SerializeField] private string testText;
+
+
+    //basic typewriter func
+    private int _currentVisibleCharacterIndex;
+    private Coroutine _typewriterCoroutine;
+
+    private WaitForSeconds _simpleDelay;
+    private WaitForSeconds _interpunctuationDelay;
+
+    [Header("Typewriter Settings")]
+    [SerializeField] private float charactersPerSecond = 20;
+    [SerializeField] private float interpunctuationDelay = 0.5f;
+
+    private void Awake()
+    {
+        _textBox = GetComponent<TMP_Text>();
+
+        _simpleDelay = new WaitForSeconds(1 / charactersPerSecond);
+        _interpunctuationDelay = new WaitForSeconds(interpunctuationDelay);
+    }
+
+    private void Start()
+    {
+        SetText(testText);
+    }
+
+    public void SetText(string text)
+    {
+        if (_typewriterCoroutine != null)
+            StopCoroutine(_typewriterCoroutine);
+
+        _textBox.text = text;
+        _textBox.maxVisibleCharacters = 0;
+        _currentVisibleCharacterIndex = 0;
+
+        _typewriterCoroutine = StartCoroutine(Typewriter());
+    }
+
+    private IEnumerator Typewriter()
+    {
+        TMP_TextInfo textInfo = _textBox.textInfo;
+
+        while (_currentVisibleCharacterIndex < textInfo.characterCount + 1)
+        {
+            char character = textInfo.characterInfo[_currentVisibleCharacterIndex].character;
+
+            _textBox.maxVisibleCharacters++;
+
+            yield return _simpleDelay;
+
+            _currentVisibleCharacterIndex++;    
+        }
+    }
+}
+
+
